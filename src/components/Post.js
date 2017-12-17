4393463879518f7a5f6d4daf8af2ea0530ee5826
Evgeni Nikolov <evgeni.tsn@gmail.com>
2017-12-18 00:08:27 +0200

    Changed: Post Component to dumb and extracted the props from it

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react'
+import React from 'react'
+import {PropTypes} from 'prop-types'
 
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Card, CardHeader, CardMedia, CardText} from 'material-ui/Card';
 
-import FlatButton from 'material-ui/FlatButton';
+const Post = ({username, date, avatar, image, description}) => {
+  //Probably will pass ID of the post for the onClick Event here
+  return (
+    <Card style={styles.cardContainer} onClick={(e)=> console.log(e.target)}>
+      <CardHeader
+        title={username}
+        subtitle={date}
+        avatar={avatar}
+      />
+      <CardMedia>
+        <img src={image} alt="" />
+      </CardMedia>
+      <CardText>{description}</CardText>
+    </Card>
+  )
+}
 
-export default class Post extends Component {
-  render() {
-    return (
-      <Card style={styles.cardContainer}>
-        <CardHeader
-          title="URL Avatar"
-          subtitle="Subtitle"
-          avatar={require('../assets/Avatar.png')}
-        />
-        <CardMedia overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}>
-          <img src={require('../assets/Sofia.jpg')} alt="" />
-        </CardMedia>
-        <CardTitle title="Card title" subtitle="Card subtitle" />
-        <CardText>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-        </CardText>
-        <CardActions>
-          <FlatButton label="Action1" />
-          <FlatButton label="Action2" />
-        </CardActions>
-      </Card>
-    )
-  }
+export default Post
+
+Post.propTypes = {
+  username: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
 }
 
 const styles = {
