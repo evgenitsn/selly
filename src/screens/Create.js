837014d6499c49e8db6e80695f434ac84881a5e0
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-01-11 15:19:41 +0200

    Reimplemented Create Screen

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,36 +1,65 @@
 import React, { Component, Fragment } from 'react'
+import TextField from 'material-ui/TextField'
+import SelectField from 'material-ui/SelectField'
+import RaisedButton from 'material-ui/RaisedButton'
+import MenuItem from 'material-ui/MenuItem'
+
 import {Header, Footer} from '../components'
-import Webcam from 'react-webcam'
+// import Webcam from 'react-webcam'
 
-export default class Feed extends Component {
+export default class Create extends Component {
   state = {
-    imageSrc: ''
-  }
-  setRef = (webcam) => {
-    this.webcam = webcam;
-  }
+    value: null,
+  };
 
-  capture = () => {
-    const imageSrc = this.webcam.getScreenshot()
-    this.setState({imageSrc: imageSrc})
-  }
+  handleChange = (event, index, value) => this.setState({value});
 
   render() {
     return (
       <Fragment>
         <Header/>
-        <div style={styles.body}>
-          <div style={styles.webcamContainer}>
-            <Webcam
-              audio={false}
-              ref={this.setRef}
-              screenshotFormat="image/jpeg"
-              width={300}
-              height={250}
+        <div style={{...styles.body, ...styles.flex}}>
+          <h4>Details</h4>
+          <div style={styles.flex}>
+            <TextField
+              floatingLabelText="Title"
+            />
+            <SelectField
+              floatingLabelText="Category"
+              value={this.state.value}
+              onChange={this.handleChange}
+            >
+              <MenuItem value={'Cat1'} primaryText="Cat1" />
+              <MenuItem value={'Cat2'} primaryText="Cat2" />
+              <MenuItem value={'Cat3'} primaryText="Cat3" />
+              <MenuItem value={'Cat4'} primaryText="Cat4" />
+            </SelectField>
+            <TextField
+              floatingLabelText="Description"
+              multiLine={true}
+              rows={3}
             />
           </div>
-          <button onClick={this.capture}>Capture photo</button>
-          <img alt="Embedded Image" src={this.state.imageSrc} />
+          <div style={styles.flex}>
+            <h4>Contacts</h4>
+            <TextField
+              floatingLabelText="Location"
+            />
+            <TextField
+              floatingLabelText="Contact Name"
+            />
+            <TextField
+              floatingLabelText="Email"
+            />
+            <TextField
+              floatingLabelText="Phone"
+            />
+          </div>
+          <RaisedButton
+            label="Create" 
+            backgroundColor="#64DD17"
+            labelColor="#fafafa"
+          />
         </div>
         <Footer/>
       </Fragment>
@@ -43,11 +72,9 @@ const styles = {
     marginTop: 80,
     marginBottom: 80,
   },
-  webcamContainer: {
-    height: 250,
-    width: '100%',
+  flex: {
     display: 'flex',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center'
   }
 }
