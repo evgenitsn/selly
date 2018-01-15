04ab784e198a3eede577a28d6781a33b473732b0
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-01-15 15:03:41 +0200

    Added Log Out button which redirects to /login

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,15 +1,33 @@
 import React, { Component, Fragment } from 'react'
-import {Header, Footer} from '../components'
+import firebase from 'firebase'
+import {Redirect} from 'react-router-dom'
+
+import RaisedButton from 'material-ui/RaisedButton'
 import Avatar from 'material-ui/Avatar'
+
+import {Header, Footer} from '../components'
 
 export default class Profile extends Component {
   render() {
+    console.log()
     return (
       <Fragment>
         <Header/>
         <div style={styles.body}>
           <div style={styles.profileHeaderContainer}>
               <Avatar size={80} src={require("../assets/Avatar.png")} />
+          </div>
+          <div style={styles.container}>
+            <RaisedButton 
+              label="Log Out" 
+              backgroundColor="#9575CD"
+              labelColor="#fafafa"
+              style={styles.registerButton} 
+              onClick={() => {
+                firebase.logout()
+                this.props.history.push('/login')
+              }}
+            />
           </div>
         </div>
         <Footer/>
@@ -21,8 +39,13 @@ export default class Profile extends Component {
 
 const styles = {
   body: {
-    marginTop: 64,
+    marginTop: 48,
     marginBottom: 80
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
   },
   profileHeaderContainer: {
       padding: 20,
@@ -31,5 +54,9 @@ const styles = {
       display: 'flex',
       justifyContent: 'space-between',
       alignItems: 'center'
-  }
+  },
+  registerButton: {
+    margin: 12, 
+    marginTop: 30,
+  },
 }
