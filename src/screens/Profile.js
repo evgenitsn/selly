c1cbd14435bfe8b2a7f9f00a1ea5dffe68d0f5c5
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-01-18 01:37:55 +0200

    Displaying the user data on Profile screen

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
+import { connect } from 'react-redux'
 
 import RaisedButton from 'material-ui/RaisedButton'
 import Avatar from 'material-ui/Avatar'
+import { Loading } from '../components';
 
-export default class Profile extends Component {
+class Profile extends Component {
   logout = () => {
     firebase.logout()
   }
 
   render() {
+    if(!this.props.firebase.profile.isLoaded) {
+      return <Loading />
+    }
     return (
       <div style={styles.body}>
         <div style={styles.profileHeaderContainer}>
-            <Avatar size={80} src={require("../assets/Avatar.png")} />
+          <Avatar size={80} src={require("../assets/Avatar.png")} />
+          <div>
+            <div>{this.props.firebase.profile.displayName}</div>
+            <br/>
+            <div>{this.props.firebase.profile.email}</div>
+          </div>
+          <div>
+            <RaisedButton 
+              label="Log Out" 
+              backgroundColor="#9575CD"
+              labelColor="#fafafa"
+              style={styles.registerButton} 
+              onClick={() => this.logout()}
+            />
+          </div>
         </div>
         <div style={styles.container}>
-          <RaisedButton 
-            label="Log Out" 
-            backgroundColor="#9575CD"
-            labelColor="#fafafa"
-            style={styles.registerButton} 
-            onClick={() => this.logout()}
-          />
+          
         </div>
       </div>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    firebase: state.firebase
+  }
+}
+
+export default connect(mapStateToProps, {})(Profile)
 
 const styles = {
   body: {
@@ -48,7 +69,6 @@ const styles = {
       alignItems: 'center'
   },
   registerButton: {
-    margin: 12, 
-    marginTop: 30,
+    margin: 12,
   },
 }
