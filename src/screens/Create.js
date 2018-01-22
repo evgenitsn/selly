ce8d663eeb9fa40d42ab1f301bac9261f49488b9
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-01-23 01:50:01 +0200

    Added Hardcoded Ad Creation and File Upload

--- src/screens/Create.js
+++ src/screens/Create.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import firebase from 'firebase'
+import { firebaseConnect } from 'react-redux-firebase'
+
 import TextField from 'material-ui/TextField'
 import SelectField from 'material-ui/SelectField'
 import RaisedButton from 'material-ui/RaisedButton'
+import FlatButton from 'material-ui/FlatButton'
 import MenuItem from 'material-ui/MenuItem'
+import { green500 } from 'material-ui/styles/colors'
 
-import { green500 } from 'material-ui/styles/colors';
-
-export default class Create extends Component {
+class Create extends Component {
   state = {
     value: null,
   };
+
+  pushSample = () => {
+    const adObj = {
+      title: 'Galaxy S8',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non lacus fermentum lectus placerat pulvinar sed a ante. Duis quis leo ipsum. Morbi in neque urna. Donec consectetur arcu quis sem malesuada, vestibulum posuere odio semper. Aenean in risus quis enim interdum venenatis at nec nibh. Nam lacinia scelerisque nulla nec pretium. Duis sit amet lorem eget tortor cursus mollis ultricies in nulla. Curabitur luctus dui ut elementum sagittis. Donec mi nunc, ultrices vel convallis ut, fringilla aliquet magna. Nulla facilisi. In sagittis tempus pharetra. In quis ligula rhoncus, rutrum mauris eget, tincidunt orci. Morbi consequat dui vel mauris faucibus, ac aliquet eros commodo.',
+      price: 600,
+      category: 'Mobile Phones',
+      condition: 'new',
+      location: 'Sofia',
+      contactName: 'Evgeni',
+      contactPhone: '0889232323',
+      user: {
+        uid: this.props.firebase.auth.uid,
+        email: this.props.firebase.auth.email,
+        displayName: this.props.firebase.auth.displayName,
+      }
+    }
+
+    firebase.push('ads', adObj)
+  }
+
+  uploadFile = (e) => {
+    console.log(e.target.files)
+    return firebase.uploadFile('uploadedFiles', e.target.files[0], 'uploadedFiles')
+  }
 
   handleChange = (event, index, value) => this.setState({value});
   // Add Stepper for the ad
@@ -52,7 +82,16 @@ export default class Create extends Component {
             floatingLabelText="Phone"
           />
         </div>
+        <FlatButton
+          label="Choose an Image"
+          labelPosition="before"
+          style={styles.uploadButton}
+          containerElement="label"
+        >
+          <input type="file" style={styles.uploadInput} onChange={(e) => this.uploadFile(e)} />
+        </FlatButton>
         <RaisedButton
+          onClick={() => this.pushSample()}
           label="Create" 
           backgroundColor={green500}
           labelColor="#fafafa"
@@ -61,6 +100,14 @@ export default class Create extends Component {
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    firebase: state.firebase
+  }
+}
+
+export default compose(firebaseConnect(['uploadedFiles']), connect(mapStateToProps, {}))(Create)
 
 const styles = {
   body: {
@@ -73,5 +120,18 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center'
-  }
+  },
+  uploadButton: {
+    verticalAlign: 'middle',
+  },
+  uploadInput: {
+    cursor: 'pointer',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    width: '100%',
+    opacity: 0,
+  },
 }
