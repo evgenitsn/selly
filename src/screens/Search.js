ee5bec11a11ce6a15ddfe5886ce4f131cdfb2859
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-01-31 00:14:05 +0200

    Added Search UI Component

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
+import SearchBar from 'material-ui-search-bar'
+
 export default class Search extends Component {
   render() {
     return (
       <div style={styles.body}>
-        SEARCH
+          <SearchBar
+            onChange={() => console.log('onChange')}
+            onRequestSearch={() => console.log('onRequestSearch')}
+            style={{
+              margin: '0 auto',
+              maxWidth: 800,
+              margin: 15
+            }}
+          />
       </div>
     )
   }
@@ -12,10 +22,11 @@ export default class Search extends Component {
 
 const styles = {
   body: {
+    backgroundColor: '#6BE3CE',
+    height: '100vh',
     margin: '0 auto',
     maxWidth: 500,
-    marginTop: 64,
-    marginBottom: 80
+    paddingTop: 55
   },
   profileHeaderContainer: {
       padding: 20,
