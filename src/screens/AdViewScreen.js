d561da9c5366e6ed4f19ef72cc41fa174c6c54dd
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-02-10 22:49:22 +0200

    Linked the details view to Firebase

--- src/screens/AdViewScreen.js
+++ src/screens/AdViewScreen.js
@@ -1,54 +1,68 @@
 import React, { Component } from 'react'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firebaseConnect } from 'react-redux-firebase'
 import { Loading } from '../components'
 import Paper from 'material-ui/Paper'
 import Chip from 'material-ui/Chip'
 
-export default class AdViewScreen extends Component {
+class AdViewScreen extends Component {
   render() {
-    console.log(this.props)
-    let { category, contactName, createdOn, description, itemCondition, location, price, title, contactPhone } = this.props.location.state.ad.details
-    return (
-      <div style={styles.outerContainer}>
-        
-        <div style={styles.innerContainer}>
-          <Paper style={styles.paper} zDepth={1}>
-            <div>
-              <h1 style={styles.title}>{title}</h1>
-              <div style={{display: 'flex', justifyContent: 'space-between', paddingTop: 10}}>
-                <h2 style={styles.price}>{price + ' лв.'}</h2>
-                <div>{new Date(createdOn).toDateString()}</div>
+    if(this.props.singleAd === undefined) {
+      return <Loading />
+    } else {
+      let { category, contactName, createdOn, description, itemCondition, location, price, title, contactPhone } = this.props.singleAd
+      return (
+        <div style={styles.outerContainer}>
+          <div style={styles.innerContainer}>
+            <Paper style={styles.paper} zDepth={1}>
+              <div>
+                <h1 style={styles.title}>{title}</h1>
+                <div style={{display: 'flex', justifyContent: 'space-between', paddingTop: 10}}>
+                  <h2 style={styles.price}>{price + ' лв.'}</h2>
+                  <div>{new Date(createdOn).toDateString()}</div>
+                </div>
               </div>
-            </div>
-            <div style={styles.chipsContainer}>
-              <Chip
-                backgroundColor={'#e2e1ff'}
-                style={styles.chip} >
-                {'Category: ' + category}
-              </Chip>
-              <Chip
-                backgroundColor={itemCondition ? '#e2e1ff' : '#EF9A9A'}
-                style={styles.chip} >
-                {itemCondition ? 'New' : 'Used'}
-              </Chip>
-            </div>
-          </Paper>
-          <Paper style={styles.paper} zDepth={1}>
-            <img style={styles.image} src={require('../assets/Varna.jpg')}/>
-          </Paper>
-          <Paper style={styles.paper} zDepth={1}>
-            <div>{'Description: ' + description}</div>
-            <br/>
-            <div>{'Location: ' + location}</div>
-            <br/>
-            <div>{'Contacts: ' + contactName}</div>
-            <br/>
-            <div>{'Phone: ' + contactPhone}</div>
-          </Paper>
+              <div style={styles.chipsContainer}>
+                <Chip
+                  backgroundColor={'#e2e1ff'}
+                  style={styles.chip} >
+                  {'Category: ' + category}
+                </Chip>
+                <Chip
+                  backgroundColor={itemCondition ? '#e2e1ff' : '#EF9A9A'}
+                  style={styles.chip} >
+                  {itemCondition ? 'New' : 'Used'}
+                </Chip>
+              </div>
+            </Paper>
+            <Paper style={styles.paper} zDepth={1}>
+              <img style={styles.image} src={require('../assets/Varna.jpg')}/>
+            </Paper>
+            <Paper style={styles.paper} zDepth={1}>
+              <div>{'Description: ' + description}</div>
+              <br/>
+              <div>{'Location: ' + location}</div>
+              <br/>
+              <div>{'Contacts: ' + contactName}</div>
+              <br/>
+              <div>{'Phone: ' + contactPhone}</div>
+            </Paper>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }
   }
 }
+
+const mapStateToProps = state => ({
+  singleAd: state.firebase.data['singleAd']
+})
+
+export default compose(
+  connect(mapStateToProps),
+  firebaseConnect((props) => [{ path: `ads/${props.location.state.ad.key}`, storeAs: 'singleAd' }])
+)(AdViewScreen)
 
 const styles = {
   outerContainer: {
