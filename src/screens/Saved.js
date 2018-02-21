d5f197812ef79df0cce1705bb94b39dbddeff822
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-02-21 15:45:41 +0200

    Load Saved Ads ID's on Saved Screen

--- src/screens/Saved.js
+++ src/screens/Saved.js
@@ -1,8 +1,68 @@
 import React, { Component } from 'react'
-import { Loading } from '../components'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { firebaseConnect, populate, isLoaded, isEmpty } from 'react-redux-firebase'
+import { Loading, DisplayCard } from '../components'
 
-export default class Saved extends Component {
+class Saved extends Component {
+  renderMyAdsList() {
+    const {savedItemsByUser} = this.props
+    if (isEmpty(savedItemsByUser)) {
+      return 'Todo list is empty. Please create UX Component for that'
+    } else {
+      return Object.keys(savedItemsByUser).map((key, id) => {
+        const navigateParams = { pathname: `/ad/${key}`, state: { ad: { key, details: savedItemsByUser[key] } } }
+        return (
+          <div key={id}>{savedItemsByUser[key]}</div>
+          //<DisplayCard ads={savedItemsByUser} key={key} adKey={key} onClick={() => this.props.history.push(navigateParams)}/>
+        )
+      })
+    }
+  }
+  
+
   render() {
-    return <Loading />
+    const { savedItemsByUser } = this.props
+    if (!isLoaded(savedItemsByUser)) {
+      return <Loading />
+    }
+    return <div style={styles.body}>{this.renderMyAdsList()}</div>
+  }
+}
+
+const mapStateToProps = state => ({
+  savedItemsByUser: state.firebase.profile.saved,
+  userId: state.firebase.auth.uid
+})
+
+export default compose(
+  firebaseConnect(props => {
+    return [
+      {
+        path: `users/${props.userId}/saved`,
+        storeAs: 'savedItemsByUser',
+      }
+    ]
+  }),
+  connect(mapStateToProps)
+)(Saved)
+
+const styles = {
+  body: {
+    backgroundColor: '#6BE3CE',
+    overflow: 'auto',
+    margin: '0 auto',
+    maxWidth: 500,
+    paddingTop: 62,
+    paddingBottom: 62,
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100%',
+    height: 'auto',
+  },
+  card: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20
   }
 }
