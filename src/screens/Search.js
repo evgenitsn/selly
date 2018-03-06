eb9a7d52f89d09956f63a3e6291e4fa311d7307e
Evgeni Nikolov <evgeni.tsn@gmail.com>
2018-03-07 01:06:41 +0200

    Added client side search

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,24 +1,73 @@
 import React, { Component } from 'react'
+import { compose } from 'redux'
 import { connect } from 'react-redux'
+import { Loading, DisplayCard } from '../components'
+import { firebaseConnect, isEmpty, isLoaded } from 'react-redux-firebase'
 
 import SearchBar from 'material-ui-search-bar'
 
-export default class Search extends Component {
+class Search extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: ''
+    }
+  }
+
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt
+    })
+  }
+
+  renderMyAdsList() {
+    const ads = this.props.ads
+    if (isEmpty(ads)) {
+      return 'There is no ads. You can go and create the first one.'
+    } else {
+      let results = Object.keys(ads).map((key, id) => {
+        if (ads[key].title.toLowerCase().includes(this.state.inputValue.toLowerCase())) {
+          const navigateParams = { pathname: `/ad/${key}`, state: { ad: { key, details: ads[key] } } }
+          return <DisplayCard ads={ads} key={key} adKey={key} onClick={() => this.props.history.push(navigateParams)} />
+        }
+      })
+      results = results.filter(n => n != undefined)
+      if(results.length < 1) {
+        return <div style={{textAlign: 'center', marginTop: 20}}>No results :(</div>
+      } else {
+        return results
+      }
+    }
+  }
+
   render() {
+    const { ads } = this.props
+    if (!isLoaded(ads)) {
+      return <Loading />
+    }
     return (
       <div style={styles.body}>
         <SearchBar
-          onChange={() => console.log('onChange')}
-          onRequestSearch={() => console.log('onRequestSearch')}
+          value={this.state.inputValue}
+          onRequestSearch={() => null}
+          onChange={evt => this.updateInputValue(evt)}
           style={{
             maxWidth: 800,
             margin: 15
           }}
         />
+        {this.state.inputValue !== '' ? this.renderMyAdsList() : null}
       </div>
     )
   }
 }
+
+export default compose(
+  firebaseConnect(props => [{ path: 'ads' }]),
+  connect((state, props) => ({
+    ads: state.firebase.data.ads
+  }))
+)(Search)
 
 const styles = {
   body: {
